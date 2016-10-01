e6fcfd2be3a4363a3410e7778f5f13900543fa50
鈴木　亮平 <ryousanngataryousann@gmail.com>
2016-10-01 18:21:28 +0900

    クソみたいなゲーム

---
+++ Assets/game/playerAtack.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerAtack : MonoBehaviour {
+	public GameObject eater;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.position += new Vector3(1.5f, 0f, 0f) * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.position -= new Vector3(1.5f, 0f, 0f) * Time.deltaTime;
+		}
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Instantiate (eater,this.transform.position,Quaternion.identity);
+		}
+
+	}
+}
